c132d05c8e36214d392caa68a4039e8dc216a680
root <root@localhost.localdomain>
2020-07-14 21:48:59 +0530

    class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+
 class App extends React.Component
 {
   render() {
     return <h1>Hello, calss component</h1>
   }
 }
+
 export default App;
