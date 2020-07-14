ed52b9cbab6b824f61799df48b86a791e4b5c4c5
root <root@localhost.localdomain>
2020-07-14 21:43:23 +0530

    functional component create

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-
-const age = (a) => {
-  if(a>8){
-    return <span>You are elegible</span>
-  }else{
-    return <span>You are not elegible</span>
+class App extends React.Component
+{
+  render() {
+    return <h1>Hello, calss component</h1>
   }
 }
-
-function App() {
-  return (
-    <div className="App">
-      <h1> Hello </h1>
-      {age(19)}
-    </div>
-  );
-}
-
 export default App;
